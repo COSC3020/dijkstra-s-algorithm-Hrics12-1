16f2e616df647484986021f693ef5fd31f849fa1
Hrics12 <111898293+Hrics12@users.noreply.github.com>
2024-11-12 11:31:44 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,27 @@
 function dijkstra(graph, sourceNode) {
-    return [];
+    let distances = {};
+    let visited = new Set();
+    
+    for (let node in graph) {
+        distances[node] = Infinity;
+    }
+    distances[sourceNode] = 0;
+
+    while (Object.keys(graph).length !== visited.size) {
+        let unvisitedNodes = Object.keys(graph).filter(node => !visited.has(node));
+        let closestNode = unvisitedNodes.reduce((a, b) => distances[a] < distances[b] ? a : b);
+
+        visited.add(closestNode);
+
+        for (let neighbor in graph[closestNode]) {
+            let alternatePathDistance = distances[closestNode] + graph[closestNode][neighbor];
+            if (alternatePathDistance < distances[neighbor]) {
+                distances[neighbor] = alternatePathDistance;
+            }
+        }
+    }
+
+    return distances;
 }
+
+module.exports = dijkstra;
