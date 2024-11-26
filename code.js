c02bf7976a5498dc144a53a2f926111dece3aca3
Hrics12 <111898293+Hrics12@users.noreply.github.com>
2024-11-26 10:12:13 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,7 +1,40 @@
 function dijkstra(graph, sourceNode) {
+    if (graph.length < 2) {
+        return 0;
+    }
+    let dist = new Array(graph.length).fill(Infinity);
 
-  if(graph.length<2){
-    return 0;
-  }
-    return [];
+    dist[sourceNode] = 0;
+    let visitedNodes = new Array(graph.length).fill(false);
+    let visitedCount = 0;
+ 
+    while (visitedCount != graph.length) {
+        let min = Infinity;
+        let currentNode = -1;
+
+        for (let i = 0; i < graph.length; i++) { // Find the minimum unvisited node
+            if (!visitedNodes[i] && dist[i] < min) {
+                min = dist[i];
+                currentNode = i;
+            }
+        }
+        if (currentNode === -1) {
+            break;
+
+        }
+        visitedNodes[currentNode] = true;
+        visitedCount++;
+        
+        for (let nextNode = 0; nextNode < graph.length; nextNode++) {
+            if (graph[currentNode][nextNode] > 0) {
+                let tempDistance = dist[currentNode] + graph[currentNode][nextNode];
+                if (dist[nextNode] > tempDistance) {
+                    dist[nextNode] = tempDistance;
+                }
+            }
+        }
+    }
+    return dist;
 }
+
+ 
